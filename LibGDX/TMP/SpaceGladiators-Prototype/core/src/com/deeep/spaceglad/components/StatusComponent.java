9952e4d0ee5f1534ce0a38c5ee9965d7882f1bfb
renesteeman <steeman.rene1@gmail.com>
2020-03-11 09:03:47 +0100

    Copied example to try to test and learn how it works (it doesn't work yet)

---
+++ LibGDX/TMP/SpaceGladiators-Prototype/core/src/com/deeep/spaceglad/components/StatusComponent.java
@@ -0,0 +1,13 @@
+package com.gdx.game.components;
+
+import com.badlogic.ashley.core.Component;
+
+/**
+ * Created by Andreas on 8/10/2015.
+ */
+public class StatusComponent extends Component{
+    public boolean alive;
+    public StatusComponent(){
+        alive = true;
+    }
+}
