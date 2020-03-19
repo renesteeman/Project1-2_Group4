5a365fea2b872d650e8fa6de4cac90bf4f6b1587
renesteeman <steeman.rene1@gmail.com>
2020-03-19 14:18:09 +0100

    Better organization for pfd/docx files, found research about wind/air physics and added UIState enum

---
+++ LibGDX/core/src/com/mygdx/game/UIState.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+public enum UIState {
+    CustomizedMenu,
+    DefaultMenu,
+    GamePlay,
+    GameScreen,
+    GamUI,
+    HitWaterUI,
+    mainMenuScreen,
+    SettingsMenu,
+    StartMenu;
+}
