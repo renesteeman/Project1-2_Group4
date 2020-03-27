4225280fe0dabac75a1b38771d91b009322c4b74
renesteeman <steeman.rene1@gmail.com>
2020-03-27 18:55:03 +0100

    Mipmapping

--- GameEngine2/src/main/java/RenderEngine/Loader.java
+++ GameEngine2/src/main/java/RenderEngine/Loader.java
@@ -2,10 +2,7 @@ package RenderEngine;
 
 import Models.RawModel;
 import org.lwjgl.BufferUtils;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL15;
-import org.lwjgl.opengl.GL20;
-import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.*;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -47,6 +44,9 @@ public class Loader {
 
     public int loadTexture(String fileName){
         renderEngine.Texture texture = new renderEngine.Texture("./res/"+fileName+".png");
+        GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+        GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -1.5f);
         int textureID = texture.getTextureID();
         textures.add(textureID);
 
