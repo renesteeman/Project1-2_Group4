f78b1fb0e46678089e43e52c37ec0dafe8a88925
renesteeman <steeman.rene1@gmail.com>
2020-03-31 14:08:53 +0200

    Reworked normal generation

--- GameEngine2/src/main/java/Terrain/Terrain.java
+++ GameEngine2/src/main/java/Terrain/Terrain.java
@@ -82,7 +82,6 @@ public class Terrain {
         heights = new float[VERTEX_COUNT][VERTEX_COUNT];
         int count = VERTEX_COUNT * VERTEX_COUNT;
         float[] vertices = new float[count * 3];
-        float[] normals = new float[count * 3];
         float[] textureCoords = new float[count*2];
         int[] indices = new int[6*(VERTEX_COUNT-1)*(VERTEX_COUNT-1)];
         int vertexPointer = 0;
@@ -93,15 +92,28 @@ public class Terrain {
                 heights[j][i] = height;
                 vertices[vertexPointer*3+1] = height;
                 vertices[vertexPointer*3+2] = (float)i/((float)VERTEX_COUNT - 1) * SIZE;
-                Vector3f normal = calculateNormal(j, i, image);
-                normals[vertexPointer*3] = normal.x;
-                normals[vertexPointer*3+1] = normal.y;
-                normals[vertexPointer*3+2] = normal.z;
+
                 textureCoords[vertexPointer*2] = (float)j/((float)VERTEX_COUNT - 1);
                 textureCoords[vertexPointer*2+1] = (float)i/((float)VERTEX_COUNT - 1);
                 vertexPointer++;
             }
         }
+
+
+        float[] normals = new float[count * 3];
+        Vector3f[][] normalVectors = NormalsGenerator.generateNormals(heights);
+        vertexPointer = 0;
+        for(int i=0;i<VERTEX_COUNT;i++) {
+            for (int j = 0; j < VERTEX_COUNT; j++) {
+                Vector3f normal = normalVectors[i][j];
+                normals[vertexPointer*3] = normal.x;
+                normals[vertexPointer*3+1] = normal.y;
+                normals[vertexPointer*3+2] = normal.z;
+                vertexPointer++;
+            }
+        }
+
+
         int pointer = 0;
         for(int gz=0;gz<VERTEX_COUNT-1;gz++){
             for(int gx=0;gx<VERTEX_COUNT-1;gx++){
